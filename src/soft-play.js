1c8b945304e8a9bb0a3129dee95c4af13fe5a9ee
pooja <dhanwade.pooja2688@gmail.com>
2023-02-27 15:59:03 +0000

    soft play class

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,5 +1,44 @@
-// TODO: Create a class in this file to contain all of the logic for this exercise
+// TODO: Write your class in this file
+class SoftPlay {
+    constructor (adults, children) {
+      this.adults = adults
+      this.children = children
+    }
+  
+    occupancy () {
+      const object ={
+        adults:this.adults,
+       children: this.children
+      }
+      return object
+    }
+  
+    enter (numAdults, numChildren) {
+      const updateAdults = this.adults + numAdults
+      const updateChildren = this.children + numChildren
+      if (numAdults < numChildren) {
 
-
-// TODO: Change the undefined value below to the name of your class
-module.exports = undefined
+        return false
+      } else {
+        this.adults = updateAdults
+        this.children = updateChildren
+        return true
+      }
+    }
+  
+    leave (numAdults, numChildren) {
+      const updateAdults = this.adults - numAdults
+      const updateChildren = this.children - numChildren
+      if (numAdults < numChildren || numAdults > this.adults || updateAdults < updateChildren) {
+        return false
+      } else {
+        this.adults = updateAdults
+        this.children = updateChildren
+        return true
+      }
+    }
+  }
+  
+  // TODO: Change undefined to the name of your class
+  module.exports = SoftPlay
+  
