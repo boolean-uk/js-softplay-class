55b25e747643d240d3cea82a2e61625564cef4a4
Kat De Mey <katdemey@outlook.com>
2023-02-27 16:48:56 +0000

    passed test 1 and 2

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,5 +1,34 @@
 // TODO: Create a class in this file to contain all of the logic for this exercise
+class Softplay {
+    constructor(adults, children) {
+        this.adults = adults
+        this.children = children
+    }
 
+    occupancy(adults, children) {
+        return {
+            adults: this.adults,
+            children: this.children
+        }
+    }
+
+    //allowed to enter or not:
+    enter(numAdults, numChildren) {
+        if (numAdults < numChildren) {
+            return false
+        } else if (numAdults >= numChildren) {
+            this.adults = + numAdults
+            this.children += numChildren
+            return true
+        }
+        
+    }
+}
+
+console.log('SoftPlay:', Softplay)
+
+const enter = new Softplay(1, 1)
+console.log('enter:', enter)
 
 // TODO: Change the undefined value below to the name of your class
-module.exports = undefined
+module.exports = Softplay
