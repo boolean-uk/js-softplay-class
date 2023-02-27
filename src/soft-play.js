4bb9ed2c1c2773465042a7eb30f67914882857c0
Akis-Hajittofi <akis.hajittofi@hotmail.co.uk>
2023-02-27 17:43:48 +0000

    Core requirements done - Tests now passing for leave - 13 specs, 0 failures

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,4 +1,3 @@
-// TODO: Create a class in this file to contain all of the logic for this exercise
 class SoftPlayCount {
   // Think of this as values that change, like a let variable
   constructor(adults, children) {
@@ -16,22 +15,25 @@ class SoftPlayCount {
     } else if (numAdults >= numChildren) {
       this.children += numChildren
       this.adults += numAdults
-
-      //   totalChildren += numChildren
-      //   totalAdults += numAdults
       return true
     }
   }
 
-  /* function occupancy() {
-        const occupants = { adults: 0, children: 0 }
-      
-        occupants.adults = adults
-        occupants.children = children
-      
-        return occupants
-      } */
+  leave(numAdults, numChildren) {
+    if (numChildren > numAdults) {
+      return false
+    } else if (numAdults === 0) {
+      return false
+    } else if (numAdults + numChildren > this.adults + this.children) {
+      return false
+    } else if (this.adults - numAdults < this.children - numChildren) {
+      return false
+    } else {
+      this.adults -= numAdults
+      this.children -= numChildren
+      return true
+    }
+  }
 }
 
-// TODO: Change the undefined value below to the name of your class
 module.exports = SoftPlayCount
