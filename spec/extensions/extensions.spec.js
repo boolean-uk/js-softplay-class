de18a75e9570bcf9ef63e82502ecd1157049fff2
carolina arruda <carolinacalouroarruda@gmail.com>
2023-06-12 19:13:27 +0200

    added more scenarios

--- spec/extensions/extensions.spec.js
+++ spec/extensions/extensions.spec.js
@@ -1,6 +1,6 @@
 const Softplay = require('../../src/soft-play.js')
 
-describe('Soft Play Extensions', () => {
+describe('SoftPlay Extensions', () => {
   let sp
 
   beforeEach(() => {
@@ -10,7 +10,18 @@ describe('Soft Play Extensions', () => {
   it('If Adult or Children leave, total should remain the same', function () {
     sp.enter(3, 2)
     sp.leave(1, 0)
-    expect(sp.occupancy()).toEqual({ adults: 2, children: 2 })
-    expect(sp.total()).toEqual({ adults: 3, children: 2 })
+    sp.enter(2, 1)
+    sp.leave(1, 1)
+    expect(sp.occupancy()).toEqual({ adults: 3, children: 2 })
+    expect(sp.total()).toEqual({ adults: 5, children: 3 })
+  })
+
+  it('If Adult or Children leave, total should remain the same', function () {
+    sp.enter(3, 2)
+    sp.enter(1, 1)
+    sp.leave(1, 0)
+    sp.leave(3, 3)
+    expect(sp.occupancy()).toEqual({ adults: 0, children: 0 })
+    expect(sp.total()).toEqual({ adults: 4, children: 3 })
   })
 })
