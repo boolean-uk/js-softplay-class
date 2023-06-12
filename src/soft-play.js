53c1b3b279a36af472a77156aca0fa430846aeb4
Todd Griffin <todd.griffin61@outlook.com>
2023-06-12 14:56:09 +0100

    created Softplay class and occupancy method

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,5 +1,19 @@
 // TODO: Create a class in this file to contain all of the logic for this exercise
 
+class Softplay {
+  constructor(adults, children) {
+    this.adults = adults
+    this.children = children
+  }
+
+  occupancy() {
+    return {adults: this.adults, children: this.children}
+  }
+}
+
+const sp = new Softplay(2, 2)
+
+console.log(sp.occupancy())
 
 // TODO: Change the undefined value below to the name of your class
 module.exports = undefined
