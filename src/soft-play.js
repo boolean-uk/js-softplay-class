87470ef4beb9527e0ce77f9e1b55aff94e10b9d8
Ian Grantham <dev.iangrantham@gmail.com>
2023-06-12 17:40:31 +0100

    ext complete

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,22 +1,33 @@
 // TODO: Create a class in this file to contain all of the logic for this exercise
 class Softplay {
-    constructor(numAdults, numChildren) {
-      this.numAdults = numAdults
-      this.numChildren = numChildren
-      this.totalAdults = numAdults
-      this.totalChildren = numChildren
-    }
-  
-    occupancy() {
-      return { adults: this.numAdults, children: this.numChildren }
-    }
+  constructor(numAdults, numChildren, location, multiplier) {
+    this.numAdults = numAdults
+    this.numChildren = numChildren
+    this.totalAdults = numAdults
+    this.totalChildren = numChildren
+    this.location = location
+    this.multiplier = multiplier
+  }
 
-    total() {
-      return { adults: this.totalAdults , children: this.totalChildren}
+  occupancy() {
+    return {
+      adults: this.numAdults,
+      children: this.numChildren
     }
-  
-    enter(numAdults, numChildren) {
-      if (numAdults >= numChildren) {
+  }
+
+  total() {
+    return {
+      adults: this.totalAdults,
+      children: this.totalChildren
+    }
+  }
+
+  enter(numAdults, numChildren) {
+    if (this.location === undefined || this.multiplier === undefined) {
+      this.location = 1
+      this.multiplier = 1
+      if (numAdults >= numChildren * this.multiplier) {
         this.numAdults += numAdults
         this.numChildren += numChildren
         this.totalAdults += numAdults
@@ -26,25 +37,27 @@ class Softplay {
         return false
       }
     }
-  
-    leave(numAdults, numChildren) {
-      if (numChildren > numAdults) {
-        return false
-      } else if (this.numAdults - numAdults < this.numChildren - numChildren) {
-        return false
-      } else if (numAdults === 0 && numChildren > 1) {
-        return false
-      } else if (numAdults > this.numAdults || numChildren > this.numChildren) {
-        return false
-      } else {
-        this.numAdults -= numAdults
-        this.numChildren -= numChildren
-        return true
-      }
+  }
+
+  leave(numAdults, numChildren) {
+    if (numChildren * this.multiplier > numAdults) {
+      return false
+    } else if (
+      this.numAdults - numAdults <
+      this.numChildren - numChildren * this.multiplier
+    ) {
+      return false
+    } else if (numAdults === 0 && numChildren > 1) {
+      return false
+    } else if (numAdults > this.numAdults || numChildren > this.numChildren) {
+      return false
+    } else {
+      this.numAdults -= numAdults
+      this.numChildren -= numChildren
+      return true
     }
   }
-  
-  const sp = new Softplay(0, 0)
+}
 
-  // TODO: Change the undefined value below to the name of your class
-  module.exports = Softplay
+// TODO: Change the undefined value below to the name of your class
+module.exports = Softplay
