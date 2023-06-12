ba22d4856dc7f54338381904640cb866afc42fb6
Isabelle <isabel.tartarelli@live.it>
2023-06-12 15:11:29 +0100

    Class created and occupancy function

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,5 +1,21 @@
 // TODO: Create a class in this file to contain all of the logic for this exercise
+class Softplay {
+  constructor() {
+    this.adults = 0
+    this.children = 0
+    this.total = {
+      adults: 0,
+      children: 0
+    }
+  }
 
+  occupancy() {
+    return {
+      adults: this.adults,
+      children: this.children
+    }
+  }
+}
 
 // TODO: Change the undefined value below to the name of your class
-module.exports = undefined
+module.exports = Softplay
