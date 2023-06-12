b88511937383fc4034e60e6a62d3f0096c8971cc
Alex Shaw <alexjshaw@gmail.com>
2023-06-12 15:51:48 +0100

    removed unnecessary comments from code

--- src/soft-play.js
+++ src/soft-play.js
@@ -21,14 +21,6 @@ class SoftPlay {
   }
 
   leave(numAdults, numChildren) {
-    // fail states
-    // 1 - A child is not attempting to leave without an adult
-    // 2 - The number of adults and children left inside the center will not cause there to be more children than adults (there must be at least 1 adult for 1 child inside the center at all times)
-    // 3 - Every child leaving the soft play center is accompanied by at least 1 adult.
-    // 4 - The number of adults and children attempting to leave is not greater than the number currently inside the center
-    // 1 & 3 - numAdults < numChildren
-    // 2 - currentAdults - numAdults < currentChildren - numChildren
-    // 4 - numAdults > currentAdults || numChildren > currentChildren
     if (numAdults < numChildren) {
       return false
     } else if (
@@ -56,6 +48,9 @@ class SoftPlay {
   }
 }
 
+// TODO: Change the undefined value below to the name of your class
+module.exports = SoftPlay
+
 // let SP = new SoftPlay(0, 0)
 // console.log(SP.occupancy())
 // SP.enter(10, 5)
@@ -64,6 +59,3 @@ class SoftPlay {
 // console.log(SP.occupancy())
 // SP.leave(2, 1)
 // console.log(SP.occupancy())
-
-// TODO: Change the undefined value below to the name of your class
-module.exports = SoftPlay
