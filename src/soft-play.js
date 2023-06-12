ab385871e5c2b2d1b06297a466cd696247e5ca25
rockettdev <kjr.dev@hotmail.com>
2023-06-12 15:17:19 +0100

    completed core task

--- src/soft-play.js
+++ src/soft-play.js
@@ -9,6 +9,35 @@ class Softplay {
   occupancy() {
     return {adults: this.adults, children: this.children}
   }
+
+  enter(adults, children) {
+
+    if (adults < children) {
+      return false
+    }
+
+    else {
+      this.adults += adults
+      this.children += children
+      return true
+    }
+  }
+
+  leave(adults, children) {
+
+    this.adults -= adults
+    this.children -= children
+
+    if (adults < children || this.children > this.adults) {
+      this.adults += adults
+      this.children += children
+      return false
+    }
+
+    else {
+      return true
+    }
+  }
 }
 
 const sp = new Softplay(2, 2)
@@ -16,4 +45,4 @@ const sp = new Softplay(2, 2)
 console.log(sp.occupancy())
 
 // TODO: Change the undefined value below to the name of your class
-module.exports = undefined
+module.exports = Softplay
