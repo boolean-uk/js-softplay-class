35f6e8c2ddc7185625f0d410085cc27b4a7f0162
Satoki <satokiito@gmail.com>
2023-12-11 14:56:21 +0000

    created Softplay class and functioning occupancy() and enter() methods

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,5 +1,32 @@
 // TODO: Create a class in this file to contain all of the logic for this exercise
 
+class Softplay {
+  constructor(numAdults, numChildren) {
+    this.numAdults = numAdults
+    this.numChildren = numChildren
+  }
+
+  occupancy() {
+    return {
+      adults: this.numAdults,
+      children: this.numChildren
+    }
+  }
+
+  enter(numAdults, numChildren) {
+    if (numAdults === 0 && numChildren > 0) {
+      return false
+    } else {
+      return true
+    }
+  }
+}
+
+const sp = new Softplay(0, 0)
+// console.log(sp)
+// console.log(sp.enter())
+const test = sp.occupancy()
+console.log(test)
 
 // TODO: Change the undefined value below to the name of your class
-module.exports = undefined
+module.exports = Softplay
