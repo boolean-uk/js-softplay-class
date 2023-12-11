0d7df774cce5464deb1247bf6d62fd807f639de8
Lukas Dembicki <lad_777@hotmail.com>
2023-12-11 15:18:20 +0000

    3 main guard clauses working.

--- src/soft-play.js
+++ src/soft-play.js
@@ -3,24 +3,46 @@
 
 class SoftPlay {
   constructor(numAdults, numChildren) {
-    this.numAdults = numAdults
-    this.numChildren = numChildren
+    this.Adults = numAdults
+    this.Children = numChildren
+    this.Total = this.Adults + this.Children
   }
 
-  enter(numAdult, numChildren) {
-    if (numAdult >= numChildren) {
-      this.numAdults += numAdult
-      this.numChildren += numChildren
+  enter(numAdults, numChildren) {
+    if (numAdults >= numChildren) {
+      this.Adults += numAdults
+      this.Children += numChildren
+      this.Total = this.Adults + this.Children
     } else return console.log('Child must be accompanied by an adult.')
+  }
+
+  leave(numAdults, numChildren) {
+    const checkNumbers = {
+      Adults: this.Adults - numAdults,
+      Children: this.Children - numChildren
+    }
+
+    const totalLeaving = numAdults + numChildren
+    const totalLeft = this.Total - totalLeaving
+    console.log('checkNumbers', checkNumbers, 'total leaving:', totalLeaving)
+
+    if (numChildren > 0 && numAdults === 0) {
+      return console.log('Child cannot leave without an adult!')
+    } else if (checkNumbers.Adults < checkNumbers.Children) {
+      return console.log('Too many adults leaving')
+    } else if (totalLeaving > totalLeft) {
+      return console.log('Too many people leaving softplay centre!!')
+    } else 
+    this.Adults -= numAdults
+    this.Children -= numChildren
+    this.Total = this.Adults + this.Children
   }
 }
 
 const softPlay1 = new SoftPlay(0, 0)
-
-softPlay1.enter(1,1)
-
+softPlay1.enter(10, 10)
+softPlay1.leave(8, 8)
 console.log(softPlay1)
-
 
 // TODO: Change the undefined value below to the name of your class
 module.exports = undefined
