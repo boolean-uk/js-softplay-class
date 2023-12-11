6bb354c7196602af6e933eb48eaacb8815dfe00b
Chloe Zermatten <chloezermatten@gmail.com>
2023-12-11 15:42:12 +0000

    add function total()

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,8 +1,10 @@
 // TODO: Create a class in this file to contain all of the logic for this exercise
 class Softplay {
-  constructor(numAdults, numChildren) {
+  constructor(numAdults, numChildren, totalNumChildren, totalNumAdults) {
     this.numAdults = numAdults
     this.numChildren = numChildren
+    this.totalNumChildren = totalNumChildren
+    this.totalNumAdults = totalNumAdults
   }
 
   enter(numAdultsEntering, numChildrenEntering) {
@@ -12,6 +14,8 @@ class Softplay {
 
     this.numAdults += numAdultsEntering
     this.numChildren += numChildrenEntering
+    this.totalNumChildren += numChildrenEntering
+    this.totalNumAdults += numAdultsEntering
     return true
   }
 
@@ -39,7 +43,18 @@ class Softplay {
   occupancy() {
     return { adults: this.numAdults, children: this.numChildren }
   }
+
+  total() {
+    return {
+      totalNumAdults: this.totalNumAdults,
+      totalNumChildren: this.totalNumChildren
+    }
+  }
 }
 
+const sp = new Softplay(0, 0, 0, 0)
+sp.enter(2, 2)
+sp.leave(1, 1)
+console.log(sp.total())
 // TODO: Change the undefined value below to the name of your class
 module.exports = Softplay
