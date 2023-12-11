ae801f57c5b4733e421b306422e3d17f7f9dfead
Satoki <satokiito@gmail.com>
2023-12-11 16:14:09 +0000

    added leave() method, partially working with my initial testing

--- src/soft-play.js
+++ src/soft-play.js
@@ -16,6 +16,23 @@ class Softplay {
     }
   }
 
+  leave(numAdults, numChildren) {
+    if (numAdults < numChildren) {
+      return false
+    } else if (
+      this.numAdults - numAdults < 0 ||
+      this.numChildren - numChildren < 0
+    ) {
+      return false
+    } else if (this.numAdults - this.numChildren < this.numChildren) {
+      return false
+    } else {
+      this.numAdults -= numAdults
+      this.numChildren -= numChildren
+      return true
+    }
+  }
+
   occupancy() {
     return {
       adults: this.numAdults,
@@ -24,8 +41,16 @@ class Softplay {
   }
 }
 
+// INITIAL TEST
 const sp = new Softplay(0, 0)
-console.log(sp.enter(2, 1))
+console.log(sp.occupancy())
+
+// TEST ENTER
+console.log(sp.enter(10, 8))
+console.log(sp.occupancy())
+
+// TEST LEAVE
+console.log(sp.leave(0, 4))
 console.log(sp.occupancy())
 
 // TODO: Change the undefined value below to the name of your class
