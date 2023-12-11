945a4e53df6b9d7a3f3b2c39979a6a6748eaa89a
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-12-11 17:46:12 +0000

    core done

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,5 +1,43 @@
-// TODO: Create a class in this file to contain all of the logic for this exercise
+class SoftPlay {
+  constructor(numAdult, numChild) {
+    this.adultPop = numAdult
+    this.childPop = numChild
+  }
 
+  enter(numAdult, numChild) {
+    const loneEnteringChild = numChild > numAdult
+    if (!loneEnteringChild) {
+      this.adultPop += numAdult
+      this.childPop += numChild
+    }
+    return !loneEnteringChild
+  }
 
-// TODO: Change the undefined value below to the name of your class
-module.exports = undefined
+  leave(numAdult, numChild) {
+    const newAdultPop = this.adultPop - numAdult
+    const newChildPop = this.childPop - numChild
+
+    const loneLeavingChild = numChild > numAdult
+    const childLeftBehind = newChildPop > newAdultPop
+    const ghostPeople = newAdultPop < 0 && newChildPop < 0
+
+    const STOP_THIS_AINT_RIGHT =
+      loneLeavingChild || childLeftBehind || ghostPeople
+
+    if (!STOP_THIS_AINT_RIGHT) {
+      this.adultPop = newAdultPop
+      this.childPop = newChildPop
+    }
+
+    return !STOP_THIS_AINT_RIGHT
+  }
+
+  occupancy() {
+    return {
+      adults: this.adultPop,
+      children: this.childPop
+    }
+  }
+}
+
+module.exports = SoftPlay
