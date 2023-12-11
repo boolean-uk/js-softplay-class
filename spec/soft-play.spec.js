d86b588ff82f1437cb4b8f0a6c90fc7aa6bea472
Eduard Bissell <edd.bissell@hotmail.co.uk>
2023-12-11 17:54:43 +0000

    add: total spec

--- spec/soft-play.spec.js
+++ spec/soft-play.spec.js
@@ -78,4 +78,10 @@ describe('Soft Play', () => {
     expect(sp.leave(2, 2)).toBeTrue()
     expect(sp.occupancy()).toEqual({ adults: 0, children: 0 })
   })
+
+  it('Leaving does not decrease total count', function () {
+    sp.enter(2, 2)
+    sp.leave(2, 2)
+    expect(sp.total()).toEqual({ adults: 2, children: 2 })
+  })
 })
