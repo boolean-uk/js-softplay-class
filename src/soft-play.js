2ab6305b16566897676db045929c5dc1baed34b6
aayushlama4008 <aayushlama4008@gmail.com>
2023-12-12 00:38:03 +0100

    added total

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,22 +1,40 @@
 // TODO: Create a class in this file to contain all of the logic for this exercise
 class SoftPlay {
   constructor(numAdults, numChildren) {
-    this.numAdults = numAdults
-    this.numChildren = numChildren
+    this.adults = numAdults
+    this.children = numChildren
+    this.total = this.adults + this.children
   }
 
   enter(numAdults, numChildren) {
     if (numAdults >= numChildren) {
-      this.numAdults += numAdults
-      this.numChildren += numChildren
-    } else return console.log(`Child must be accompanied by and adult`)
+      this.adults += numAdults
+      this.children += numChildren
+      this.total = this.adults + this.children
+
+      return true
+    } else console.log(`Child must be accompanied by an adult`)
+    return false
+  }
+
+  leave(numAdults, numChildren) {
+    const checkNumbers = {
+      adults: this.adults - numAdults,
+      children: this.children - numChildren
+    }
+    const totalLeaving = numAdults + numChildren
+    const totalLeft = this.total - totalLeaving
+    console.log('checkNumbers', checkNumbers)
+    console.log('totalLeaving', totalLeaving)
+    console.log('totalLeft', totalLeft)
   }
 }
 
 const numOfPeople = new SoftPlay(0, 0)
 
-numOfPeople.enter(1, 3)
+numOfPeople.enter(4, 3)
 
+numOfPeople.leave(1, 1)
 console.log(numOfPeople)
 
 // TODO: Change the undefined value below to the name of your class
