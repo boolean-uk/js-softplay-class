8ce2093d4716ca2d8d0e545ef7d19e83c96434f1
irlydo <fokum8@gmail.com>
2023-12-12 12:25:43 +0300

    Formatted with Prettier

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,39 +1,42 @@
 class Softplay {
-    constructor(numAdults, numChildren) {
-        this.adults = numAdults;
-        this.children = numChildren;
+  constructor(numAdults, numChildren) {
+    this.adults = numAdults
+    this.children = numChildren
+  }
+
+  enter(numAdults, numChildren) {
+    if (numChildren > numAdults) {
+      return false
     }
 
-    enter(numAdults, numChildren) {
-        if (numChildren > numAdults) {
-            return false;
-        }
+    this.adults += numAdults
+    this.children += numChildren
+    return true
+  }
 
-        this.adults += numAdults;
-        this.children += numChildren;
-        return true;
+  leave(numAdults, numChildren) {
+    if (numAdults > this.adults || numChildren > this.children) {
+      return false
     }
 
-    leave(numAdults, numChildren) {
-        if (numAdults > this.adults || numChildren > this.children) {
-            return false;
-        }
-
-        if (numChildren > numAdults || (this.children - numChildren) > (this.adults - numAdults)) {
-            return false;
-        }
-
-        this.adults -= numAdults;
-        this.children -= numChildren;
-        return true;
+    if (
+      numChildren > numAdults ||
+      this.children - numChildren > this.adults - numAdults
+    ) {
+      return false
     }
 
-    occupancy() {
-        return {
-            adults: this.adults,
-            children: this.children
-        };
+    this.adults -= numAdults
+    this.children -= numChildren
+    return true
+  }
+
+  occupancy() {
+    return {
+      adults: this.adults,
+      children: this.children
     }
+  }
 }
 
 module.exports = Softplay
