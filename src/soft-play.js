c94547223aae019b3d3390529361551a14f49640
Atif Furqaan <atifurqaan@gmail.com>
2023-12-13 09:52:50 +0000

    Corrected the errors

--- src/soft-play.js
+++ src/soft-play.js
@@ -66,19 +66,10 @@ class SoftPlayCentres {
 const spc = new SoftPlayCentres()
 spc.createSoftPlay(0, 0, 1)
 spc.createSoftPlay(0, 0, 2)
-console.log(spc.getAllSoftPlay())
-
 spc.getSoftPlayByIndex(0).enter(2, 1)
 spc.getSoftPlayByIndex(0).leave(1, 0)
-console.log(spc.getAllSoftPlay())
-
 spc.getSoftPlayByIndex(1).enter(4, 1)
-console.log(spc.getSoftPlayByIndex(1).occupancy())
 spc.getSoftPlayByIndex(1).leave(2, 0)
-console.log(spc.getSoftPlayByIndex(1).occupancy())
 spc.getSoftPlayByIndex(1).leave(2, 1)
-console.log(spc.getSoftPlayByIndex(1).occupancy())
-console.log(spc.getSoftPlayByIndex(1).total())
-
 // TODO: Change the undefined value below to the name of your class
 module.exports = Softplay
