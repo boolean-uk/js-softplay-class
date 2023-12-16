e8facb6882a58ffdf5d117bc93686d4233160e05
Pierluigi <pierluigi.capirci89@gmail.com>
2023-12-16 12:26:30 +0000

    create class Softplay

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,5 +1,10 @@
 // TODO: Create a class in this file to contain all of the logic for this exercise
-
+class Softplay {
+  constructor(numAdults, numChildren) {
+    this.numAdults = numAdults
+    this.numChildren = numChildren
+  }
+}
 
 // TODO: Change the undefined value below to the name of your class
-module.exports = undefined
+module.exports = Softplay
