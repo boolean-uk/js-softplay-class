5b18a1948375f7862226fca5f07532bda28d739e
SukunimiVinod <vinodvinod19032022@gmail.com>
2023-12-19 14:48:54 +0200

    error fixed

--- src/soft-play.js
+++ src/soft-play.js
@@ -5,41 +5,41 @@ class Softplay {
     this.numChildren = numChildren
   }
 
-    enter(numAdults, numChildren) {
-      if (numChildren < 1 || numAdults < 1 || numChildren > numAdults) {
-        return false
-      } else {
-        this.numAdults += numAdults
-        this.numChildren += numChildren
-        return true
-      }
-    }
-  
-    leave(numAdults, numChildren) {
-      if (numAdults < numChildren) {
-        return false
-      } else if (
-        this.numAdults - numAdults < 0 ||
-        this.numChildren - numChildren < 0
-      ) {
+  enter(numAdults, numChildren) {
+    if (numChildren < 1 || numAdults < 1 || numChildren > numAdults) {
       return false
-      } else if (this.numAdults - numAdults < this.numChildren - numChildren) {
-        return false
-      } else {
-        this.numAdults -= numAdults
-        this.numChildren -= numChildren
-        return true
-      }
-    }
-  
-    occupancy() {
-      return {
-        adults: this.numAdults,
-        children: this.numChildren
-      }
+    } else {
+      this.numAdults += numAdults
+      this.numChildren += numChildren
+      return true
     }
   }
-  
-  const sp = new Softplay()
-  
-  module.exports = Softplay
+
+  leave(numAdults, numChildren) {
+    if (numAdults < numChildren) {
+      return false
+    } else if (
+      this.numAdults - numAdults < 0 ||
+      this.numChildren - numChildren < 0
+    ) {
+      return false
+    } else if (this.numAdults - numAdults < this.numChildren - numChildren) {
+      return false
+    } else {
+      this.numAdults -= numAdults
+      this.numChildren -= numChildren
+      return true
+    }
+  }
+
+  occupancy() {
+    return {
+      adults: this.numAdults,
+      children: this.numChildren
+    }
+  }
+}
+
+const sp = new Softplay()
+
+module.exports = Softplay
