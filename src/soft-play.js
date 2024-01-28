0094af7ae4ceb0b5248c05a93ad6768790b02ff1
Mobeen <muhammadmobeen1@hotmail.com>
2024-01-28 11:28:49 +0100

    done

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,5 +1,45 @@
 // TODO: Create a class in this file to contain all of the logic for this exercise
-
-
-// TODO: Change the undefined value below to the name of your class
-module.exports = undefined
+class Softplay {
+    constructor(numAdults, numChildren) {
+      this.numAdults = numAdults
+      this.numChildren = numChildren
+    }
+  
+    enter(numAdults, numChildren) {
+      if (numChildren < 1 || numAdults < 1 || numChildren > numAdults) {
+        return false
+      } else {
+        this.numAdults += numAdults
+        this.numChildren += numChildren
+        return true
+      }
+    }
+  
+    leave(numAdults, numChildren) {
+      if (numAdults < numChildren) {
+        return false
+      } else if (
+        this.numAdults - numAdults < 0 ||
+        this.numChildren - numChildren < 0
+      ) {
+        return false
+      } else if (this.numAdults - numAdults < this.numChildren - numChildren) {
+        return false
+      } else {
+        this.numAdults -= numAdults
+        this.numChildren -= numChildren
+        return true
+      }
+    }
+  
+    occupancy() {
+      return {
+        adults: this.numAdults,
+        children: this.numChildren
+      }
+    }
+  }
+  
+  const sp = new Softplay()
+  
+  module.exports = Softplay
