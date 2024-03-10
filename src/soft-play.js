44e02df2418eeca756eb3f7a2c9f3cb284be4743
Mobeen <muhammadmobeen1@hotmail.com>
2024-03-10 08:12:52 +0100

    updated

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,59 +1,80 @@
-// src/soft-play.js
-
 class Softplay {
   constructor(numAdults, numChildren) {
-    this.totalAdults = 0;
-    this.totalChildren = 0;
-    this.currentAdults = numAdults;
-    this.currentChildren = numChildren;
+    this.numAdults = numAdults
+    this.numChildren = numChildren
+    this.totalAdults = this.numAdults
+    this.totalChildren = this.numChildren
   }
 
+  // enter
   enter(numAdults, numChildren) {
-    // Check if every child entering is accompanied by at least 1 adult
-    if (numChildren > 0 && numAdults === 0) {
-      return false;
+    if (numAdults < numChildren) {
+      return false
+    } else {
+      this.numAdults += numAdults
+      this.numChildren += numChildren
+      this.totalAdults += numAdults
+      this.totalChildren += numChildren
+      return true
     }
-
-    this.currentAdults += numAdults;
-    this.currentChildren += numChildren;
-    this.totalAdults += numAdults;
-    this.totalChildren += numChildren;
-
-    return true;
   }
 
+  // leave
   leave(numAdults, numChildren) {
-    // Check if a child is not attempting to leave without an adult
-    // Check if the number of adults and children left inside the center will not cause more children than adults
-    // Check if every child leaving is accompanied by at least 1 adult
-    // Check if the number of adults and children attempting to leave is not greater than the number currently inside the center
-    if (
-      numChildren > 0 &&
-      numAdults === 0 &&
-      this.currentAdults - numAdults < this.currentChildren - numChildren + 1
+    if (numAdults < numChildren) {
+      return false
+    } else if (
+      this.numAdults - numAdults < 0 ||
+      this.numChildren - numChildren < 0
     ) {
-      return false;
+      return false
+    } else if (this.numAdults - numAdults < this.numChildren - numChildren) {
+      return false
+    } else {
+      this.numAdults -= numAdults
+      this.numChildren -= numChildren
+      return true
     }
-
-    this.currentAdults -= numAdults;
-    this.currentChildren -= numChildren;
-
-    return true;
   }
 
+  // occupacy
   occupancy() {
     return {
-      adults: this.currentAdults,
-      children: this.currentChildren,
-    };
+      adults: this.numAdults,
+      children: this.numChildren
+    }
   }
 
+  // total
   total() {
     return {
       adults: this.totalAdults,
-      children: this.totalChildren,
-    };
+      children: this.totalChildren
+    }
   }
 }
 
-module.exports = Softplay;
+const sp = new Softplay(0, 0)
+
+console.log(sp.occupancy())
+// Output: { adults: 0, children: 0 }
+console.log(sp.enter(10, 6))
+// Output: true
+console.log(sp.occupancy())
+// Output: { adults: 10, children: 6 }
+console.log(sp.enter(3, 3))
+// Output: true
+console.log(sp.occupancy())
+// Output: { adults: 13, children: 9 }
+console.log(sp.leave(5, 4))
+// Output: true
+console.log(sp.occupancy())
+// Output: { adults: 8, children: 5 }
+console.log(sp.enter(8, 5))
+// Output: true
+console.log(sp.occupancy())
+// Output: { adults: 16, children: 10 }
+console.log(sp.total())
+
+// TODO: Change the undefined value below to the name of your class
+module.exports = Softplay
