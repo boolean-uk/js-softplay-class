594232a1bbedcf59b0c9273ec22f802037f30652
Will Baxter <bax.will@googlemail.com>
2024-05-28 13:42:31 +0100

    Completed core criteria

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,5 +1,42 @@
+/* eslint-disable prettier/prettier */
 // TODO: Create a class in this file to contain all of the logic for this exercise
 
+class Softplay {
+  constructor(numAdults, numChildren) {
+    this.numAdults = numAdults
+    this.numChildren = numChildren
+  }
+
+  enter(numAdults, numChildren) {
+    if (numAdults < numChildren) {
+      return false
+    }
+    this.numAdults += numAdults
+    this.numChildren += numChildren
+    return true
+  }
+
+  leave(numAdults, numChildren) {
+    if (
+      !numAdults ||
+      this.numAdults - numAdults < this.numChildren - numChildren ||
+      numChildren > numAdults ||
+      (this.numAdults - numAdults < 0 || this.numChildren - numChildren < 0)
+    ) {
+      return false
+    }
+    this.numAdults -= numAdults
+    this.numChildren -= numChildren
+    return true
+  }
+  
+  occupancy() {
+    return {
+        adults: this.numAdults,
+        children: this.numChildren
+    }
+  }
+}
 
 // TODO: Change the undefined value below to the name of your class
-module.exports = undefined
+module.exports = Softplay
