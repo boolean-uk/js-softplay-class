ac515b7d51ec72d999eb6088c875af5334c379df
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-05-28 13:08:30 +0200

    create base softplay class

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,5 +1,43 @@
 // TODO: Create a class in this file to contain all of the logic for this exercise
 
+class Softplay {
+  constructor(numAdults, numChildren) {
+    this.numAdults = numAdults
+    this.numChildren = numChildren
+  }
+
+  enter(numAdults, numChildren) {
+    if (numAdults >= numChildren) {
+      this.numAdults += numAdults
+      this.numChildren += numChildren
+      return true
+    } else {
+      return false
+    }
+  }
+
+  occupancy() {
+    return {
+      adults: this.numAdults,
+      children: this.numChildren
+    }
+  }
+
+  leave(numAdults, numChildren) {
+    if (
+      numChildren <= numAdults &&
+      this.numChildren - numChildren <= this.numAdults - numAdults &&
+      numChildren <= this.numChildren &&
+      numAdults <= this.numAdults
+    ) {
+      this.numAdults -= numAdults
+      this.numChildren -= numChildren
+      return true
+    } else {
+      return false
+    }
+  }
+}
 
 // TODO: Change the undefined value below to the name of your class
-module.exports = undefined
+module.exports = Softplay
