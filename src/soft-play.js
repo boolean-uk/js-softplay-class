b8832de542ea62fbfc01cc624d7621cfa94ebcaa
JOANNABUUMA1 <nabuumaj6@gmail.com>
2024-05-29 03:40:27 +0100

    enter mothod

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,5 +1,20 @@
 // TODO: Create a class in this file to contain all of the logic for this exercise
 
+class Softplay {
+  constructor(numAdults = 0, numChildren = 0) {
+    this.numAdults = numAdults
+    this.numChildren = numChildren
+  }
 
+  enter(numAdults, numChildren) {
+    if (numAdults / numChildren < 1) {
+      return false
+    }
+
+    this.numAdults += numAdults
+    this.numChildren += numChildren
+    return true
+  }
+}
 // TODO: Change the undefined value below to the name of your class
-module.exports = undefined
+module.exports = Softplay
