40bf46028c34eadbec1ace1ae27efc0027841e7b
JOANNABUUMA1 <nabuumaj6@gmail.com>
2024-05-29 08:22:20 +0100

    leave method

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,9 +1,13 @@
+/* eslint-disable prettier/prettier */
 // TODO: Create a class in this file to contain all of the logic for this exercise
 
 class Softplay {
   constructor(numAdults = 0, numChildren = 0) {
     this.numAdults = numAdults
     this.numChildren = numChildren
+
+    this.totalAdults = numAdults
+    this.totalChildren = numChildren
   }
 
   enter(numAdults, numChildren) {
@@ -13,7 +17,29 @@ class Softplay {
 
     this.numAdults += numAdults
     this.numChildren += numChildren
+
+    this.numAdults += numAdults
+    this.numChildren += numChildren
     return true
+  }
+
+  leave(numAdults, numChildren) {
+    if (numAdults === 0 && numChildren > 0) {
+      return false
+    } else if (
+      ((this.numAdults - numAdults) / (this.numChildren - numChildren) <
+      1)) {
+      return false
+    } else if ((numAdults / numChildren) <1) {
+        return false
+    } else if ((numAdults + numChildren > this.numAdults + this.numChildren)) {
+        return false
+    } else {
+
+        this.numAdults -= numAdults
+        this.numChildren -= numChildren
+        return true
+    }
   }
 }
 // TODO: Change the undefined value below to the name of your class
