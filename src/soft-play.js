22815043e82e671771e43c91bb982b1d9b17a66f
JOANNABUUMA1 <nabuumaj6@gmail.com>
2024-05-29 19:13:56 +0100

    bug fix for core criteria and extensions

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,8 +1,7 @@
-/* eslint-disable prettier/prettier */
 // TODO: Create a class in this file to contain all of the logic for this exercise
 
 class Softplay {
-  constructor(numAdults = 0, numChildren = 0) {
+  constructor(numAdults, numChildren) {
     this.numAdults = numAdults
     this.numChildren = numChildren
 
@@ -18,8 +17,8 @@ class Softplay {
     this.numAdults += numAdults
     this.numChildren += numChildren
 
-    this.numAdults += numAdults
-    this.numChildren += numChildren
+    this.totalAdults += numAdults
+    this.totalChildren += numChildren
     return true
   }
 
@@ -27,26 +26,32 @@ class Softplay {
     if (numAdults === 0 && numChildren > 0) {
       return false
     } else if (
-      ((this.numAdults - numAdults) / (this.numChildren - numChildren) <
-      1)) {
+      (this.numAdults - numAdults) / (this.numChildren - numChildren) <
+      1
+    ) {
       return false
-    } else if ((numAdults / numChildren) <1) {
-        return false
-    } else if ((numAdults + numChildren > this.numAdults + this.numChildren)) {
-        return false
+    } else if (numAdults / numChildren < 1) {
+      return false
+    } else if (numAdults + numChildren > this.numAdults + this.numChildren) {
+      return false
     } else {
-
-        this.numAdults -= numAdults
-        this.numChildren -= numChildren
-        return true
+      this.numAdults -= numAdults
+      this.numChildren -= numChildren
+      return true
     }
   }
-   
-  occupancy(){
-    return {
-        adults: this.numAdults,
-        children: this.numChildren
 
+  occupancy() {
+    return {
+      adults: this.numAdults,
+      children: this.numChildren
+    }
+  }
+
+  total() {
+    return {
+      adults: this.totalAdults,
+      children: this.totalChildren
     }
   }
 }
